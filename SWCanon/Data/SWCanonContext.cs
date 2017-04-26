dfcb9c57c1ad70e24334e765884e7d3c4c3153a8
Éamonn Reilly <eamonnreilly87@hotmail.com>
2017-04-26 17:10:07 +0100

    Init

---
+++ SWCanon/Data/SWCanonContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace SWCanon.Models
+{
+    public class SWCanonContext : DbContext
+    {
+        public SWCanonContext (DbContextOptions<SWCanonContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SWCanon.Models.Movie> Movie { get; set; }
+    }
+}
